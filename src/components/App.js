0758a8ca95b7473a577465acf0cfdbcd8b12d716
Harshal Atkar <142751341+HarshalAtkar@users.noreply.github.com>
2024-03-22 12:21:51 +0000

    added

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,18 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  
+  const count=0;
+  
+  increment(){
+     count=count+1;
+  }
   return (
+  
+
     <div>
-        {/* Do not remove the main div */}
+       <p>Button clicked {this.count} times</p>
+       <button onClick={this.increment}>Increment</button>
     </div>
   )
 }
